befc2ff62756b81554f021a6bd36a57587405144
Olivier Monod <olivier.ducimetierealiasmonod@bit.admin.ch>
2017-06-14 12:53:26 +0000

    next try

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,9 +3,11 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("hello my brave new world (changed message text)");
-		if (args[0] == null)
+		if (args.length == 0)
 		{
-			System.out.println("What do you mean by "+args[0]+"?");
+			System.out.println("\nNothing to say?");
+		} else {
+			System.out.println("\nWhat do you mean by "+args[0]+"?");
 		}
 	}
 }
