6456ecb44aa8ca73e58c68fa88b5d5377f3dc550
Olivier Monod <olivier.ducimetierealiasmonod@bit.admin.ch>
2017-06-14 12:00:31 +0000

    question added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("hello my brave new world (changed message text)");
+		System.out.println("What do you mean by "+args[0]+"?");
 	}
 }
