ba738722b548ebede5945fce339520e3f1c15797
Olivier Monod <olivier.ducimetierealiasmonod@bit.admin.ch>
2017-06-14 12:27:46 +0000

    changed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld
 	public static void main (String [] args)
 	{
 		System.out.println("hello my brave new world (changed message text)");
-		System.out.println("What do you mean by "+args[0]+"?");
+		if (args[0] == null)
+		{
+			System.out.println("What do you mean by "+args[0]+"?");
+		}
 	}
 }
